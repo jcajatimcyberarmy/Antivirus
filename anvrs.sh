423b603b5553169d546b6bc14fb0e657495a8a74
Jatim Cyber Army <49344283+jcajatimcyberarmy@users.noreply.github.com>
2019-04-22 00:54:41 +0700

    Add files via upload

---
+++ anvrs.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo $green"please!! Login Your Account"
+echo $green"*******************"
+read -p "Username : " username
+read -p "Password : " password
+echo $green"*******************"
+echo
+echo $red"==>> Welcome $green$username <<=="
+sleep 1
+echo $red"==>>  $greenTools Antivirus  $red<<=="
+sleep 1
+echo $red"==>>$greenCreated By Mr.Tuman$red<<=="
+sleep 1
+echo $red"==>> $green•Jatim Cyber Army $red<<=="
+sleep 1
+echo ""
+echo $red"++++++++++++++++++++++++++++++"
+echo $red"$green+Masukkan Jumlah Keluhan kamu+"
+echo $red"$green+Misal :                     +"
+echo $red"$green+-Ngelag                     +"
+echo $red"$green+-Jaringan Lemot             +"
+echo $red"$green+Dan Masukkan Jumlah Nya 2   +"
+echo $red"++++++++++++++++++++++++++++++"
+read -p "Jumlah Keluhan Kamu : " keluhan
+sleep 2
+echo ""
+echo $green"Search Virus in Your Device?"
+read -p "y/n : " yn
+sent=1
+clear
+echo "[-           ]"
+sleep 1
+clear
+echo "[--          ]"
+sleep 1
+clear
+echo "[---         ]"
+sleep 1
+clear
+echo "[----        ]"
+sleep 1
+clear
+echo "[-----       ]"
+sleep 1
+clear
+echo "[-----       ]"
+sleep 1
+clear
+echo "[------      ]"
+sleep 1
+clear
+echo "[-------     ]"
+sleep 1
+clear
+echo "[--------    ]"
+sleep 1
+clear
+echo "[---------   ]"
+sleep 1
+clear
+echo "[----------  ]"
+sleep 1
+clear
+echo "[----------- ]"
+sleep 1
+clear
+echo "[------------]"
+sleep 1
+clear
+echo "[#-----------]"
+sleep 1
+clear
+echo "[##----------]"
+sleep 1
+clear
+echo "[###---------]"
+sleep 1
+clear
+echo "[####--------]"
+sleep 1
+clear
+echo "[#####-------]"
+sleep 1
+clear
+echo "[######------]"
+sleep 1
+clear
+echo "[#######-----]"
+sleep 1
+clear
+echo "[########----]"
+sleep 1
+clear
+echo "[#########---]"
+sleep 1
+clear
+echo "[##########--]"
+sleep 1
+clear
+echo "[###########-]"
+sleep 1
+clear
+echo "[############]"
+sleep 3
+clear
+echo $green"[+] Loading Sucsess [+]"
+echo ""
+echo $red"$keluhan Virus Detected [÷]"
+sleep 2
+clear
+sh antidos.sh
+
+
+
+
+
+
+
+
+
+
